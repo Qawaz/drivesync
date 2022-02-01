34dc08fd18be7502045feb49db9dbc6cd2321428
Waqas Tahir <wakaztahir@gmail.com>
2022-02-01 19:56:28 +0000

    drivesync git url changed

--- drivesync/build.gradle.kts
+++ drivesync/build.gradle.kts
@@ -90,7 +90,7 @@ afterEvaluate {
         repositories {
             maven {
                 name = "GithubPackages"
-                url = uri("https://maven.pkg.github.com/timeline-notes/drivesync")
+                url = uri("https://maven.pkg.github.com/codeckle/drivesync")
 
                 credentials {
                     username = (githubProperties["gpr.usr"] ?: System.getenv("GPR_USER")).toString()
